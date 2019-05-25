605247d4874cea2cc61029ccd45926c659b5acd5
PGboy <zscfde@gmail.com>
2019-05-26 00:48:27 +0800

    feature(Layout): add Layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,14 @@
 import React from 'react';
 import './App.css';
+import { TodoListApp } from './container/TodoListApp';
+import Layout from './shared/layouts/Layout';
 
-import { TodoListApp } from './container/TodoListApp'
-
-const App: React.FC = () => <TodoListApp />;
+const App: React.FC = () => {
+    return (
+        <Layout>
+            <TodoListApp></TodoListApp>
+        </Layout>
+    )
+};
 
 export default App;
